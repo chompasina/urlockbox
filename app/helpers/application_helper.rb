7de216768a4cc8d60746888cbf2e49bcd3790e20
Tommasina <tommasina1@gmail.com>
2016-11-08 09:22:15 -0700

    Changes hyperlink styling based on status using helper method. Now to do it in jQuery...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
       return "Mark as Unread"
     end
   end
+  
+  def link_style(link)
+    if link.read
+       "class=link-read"
+    else
+      "class=link-unread"
+    end
+  end
 end
